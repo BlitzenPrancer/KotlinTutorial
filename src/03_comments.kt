1fd513b082b0e5310fb6e2ce9e9056afed1d103c
SUDARSHAN R <blitzenprancer666@gmail.com>
2021-10-28 19:53:53 +0530

    Update 03_comments.kt

--- src/03_comments.kt
+++ src/03_comments.kt
@@ -5,10 +5,4 @@
 * This is comment line 1
 *
 * This is comment line 2
-*
-* This is main function. Entry point of the application. 
 * */
-
-fun main(args: Array<String>) {     // This is inline comment ...
-    print("Hello World")
-}
