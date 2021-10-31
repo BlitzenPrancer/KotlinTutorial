ac659644f9d2d1bf20c1b4f8d0b0763328793aba
SUDARSHAN R <blitzenprancer666@gmail.com>
2021-10-31 22:11:42 +0530

    Update 31_interface.kt

--- src/31_interface.kt
+++ src/31_interface.kt
@@ -1,43 +1,14 @@
+interface MyInterface {
+    val prop: Int // abstract
 
-/*
-*   INTERFACE
-* */
-fun main(args: Array<String>) {
+    val propertyWithImplementation: String
+        get() = "foo"
 
-    var myButton = MyButton()
-    myButton.onTouch()
-    myButton.onClick()
-}
-
-interface MyInterfaceListener {     // You cannot create the instance of interface
-
-    fun onTouch()                   // Methods in interface are abstract by default
-
-    fun onClick() {                 // Normal methods are public and open by default but NOT FINAL
-        println("MyInterfaceListener: onClick")
+    fun foo() {
+        print(prop)
     }
 }
 
-interface MySecondInterface {       // You cannot create the instance of interface
-
-    fun onTouch() {                 // Normal Method
-        println("MySecondInterface: onTouch")
-    }
-
-    fun onClick() {                 // Normal methods are public and open by default but NOT FINAL
-        println("MySecondInterface: onClick")
-    }
-
-}
-
-class MyButton: MyInterfaceListener, MySecondInterface {
-
-    override fun onTouch() {
-        super<MyInterfaceListener>.onClick()
-        super<MySecondInterface>.onClick()
-    }
-
-    override fun onClick() {
-        super.onTouch()
-    }
+class Child : MyInterface {
+    override val prop: Int = 29
 }
