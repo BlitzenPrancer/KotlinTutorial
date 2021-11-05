60d628e0bb5d2e1ffe4c83e9f9c318ae585c6e4a
SUDARSHAN R <blitzenprancer666@gmail.com>
2021-11-05 20:13:19 +0530

    Update 24_infix_function.kt

--- src/24_infix_function.kt
+++ src/24_infix_function.kt
@@ -1,27 +1,8 @@
-
-/*
-*   INFIX FUNCTIONS
-* */
-fun main(args: Array<String>) {
-
-    val x: Int = 6
-    val y: Int = 10
-
-    val greaterVal = x findGreaterValue y   // x.findGreaterValue(y)
-
-    println(greaterVal)
+infix fun String.substringMatches(r: Regex) : List<String> {
+    return r.findAll(this)
+      .map { it.value }
+      .toList()
 }
 
-infix fun Int.findGreaterValue(other: Int): Int {       // INFIX and Extension Func
-
-    if (this > other)
-        return this
-    else
-        return other
-}
-
-/*
-*       1. All INFIX Functions are Extension functions
-*            But all Extension functions are not INFIX
-*       2. INFIX Functions just have ONE PARAMETER
-* */
+val matches = "a bc def" substringMatches ".*? ".toRegex()
+Assert.assertEquals(listOf("a ", "bc "), matches)
